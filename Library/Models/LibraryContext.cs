4b8a92f6ac01f6735cbc24a2516adaf9a4cd1ac5
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-03-25 10:23:03 -0700

    add and update LibraryContext.cs file.

---
+++ Library/Models/LibraryContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Models
+{
+  public class LibraryContext : DbContext
+  {
+    public virtual DbSet<Book> Books { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<BookAuthor> BookAuthor { get; set; }
+
+    public LibraryContext(DbContextOptions options) : base(options) { }
+  }
+}
