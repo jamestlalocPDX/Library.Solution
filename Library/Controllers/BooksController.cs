625e1801ca0252ea20a35d0ce15e42f8677fd514
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-03-25 10:21:32 -0700

    add and update BooksController.cs file.

---
+++ Library/Controllers/BooksController.cs
@@ -0,0 +1,75 @@
+using Microsoft.AspNetCore.Mvc;
+using Library.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Controllers
+{
+  public class BooksController : Controller
+  {
+    private readonly LibraryContext _db;
+
+    public BooksController(LibraryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Book> model = _db.Books.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Book book)
+    {
+      _db.Books.Add(book);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Book thisBook = _db.Books
+          .Include(book => book.Authors)
+          .ThenInclude(join => join.Author)
+          .FirstOrDefault(book => book.BookId == id);
+      return View(thisBook);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisBook = _db.Books.FirstOrDefault(book => book.BookId == id);
+      return View(thisBook);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Book book)
+    {
+      _db.Entry(book).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Book thisBook = _db.Books.FirstOrDefault(book => book.BookId == id);
+      return View(thisBook);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisBook = _db.Books.FirstOrDefault(book => book.BookId == id);
+      _db.Books.Remove(thisBook);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
