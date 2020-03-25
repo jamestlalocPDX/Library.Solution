8a8d759a65a1dd1f3cf73b92cedac145de5d017e
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-03-25 15:31:50 -0700

    fix indentation.

--- Library/Startup.cs
+++ Library/Startup.cs
@@ -35,12 +35,12 @@ namespace Library
 
       services.Configure<IdentityOptions>(options =>
       {
-          options.Password.RequireDigit = false;
-          options.Password.RequiredLength = 0;
-          options.Password.RequireLowercase = false;
-          options.Password.RequireNonAlphanumeric = false;
-          options.Password.RequireUppercase = false;
-          options.Password.RequiredUniqueChars = 0;
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 0;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredUniqueChars = 0;
       });
     }
 
