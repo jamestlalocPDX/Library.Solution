24bdcad23dbe84f11f4a8bf7d7903e6125758190
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-03-25 15:30:39 -0700

    update Startup.cs config to override default user requirements.

--- Library/Startup.cs
+++ Library/Startup.cs
@@ -29,9 +29,19 @@ namespace Library
         .AddDbContext<LibraryContext>(options => options
         .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
 
-        services.AddIdentity<ApplicationUser, IdentityRole>()
-          .AddEntityFrameworkStores<LibraryContext>()
-          .AddDefaultTokenProviders();
+      services.AddIdentity<ApplicationUser, IdentityRole>()
+        .AddEntityFrameworkStores<LibraryContext>()
+        .AddDefaultTokenProviders();
+
+      services.Configure<IdentityOptions>(options =>
+      {
+          options.Password.RequireDigit = false;
+          options.Password.RequiredLength = 0;
+          options.Password.RequireLowercase = false;
+          options.Password.RequireNonAlphanumeric = false;
+          options.Password.RequireUppercase = false;
+          options.Password.RequiredUniqueChars = 0;
+      });
     }
 
     public void Configure(IApplicationBuilder app)
